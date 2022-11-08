5e1cabce23d974c5448c7c1ab41dd27014a6f157
bandarichandana-2926 <bandarichandana96@gmail.com>
2022-11-08 13:10:00 +0530

    update

---
+++ Form-validation/one.js
@@ -0,0 +1,7 @@
+function myform(){
+    let a1 = document.getElementById('name').value 
+    
+    if(==""){
+        document.getElementById("usermessage").innerHTML="please enter the name"
+    }
+}
