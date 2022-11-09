9402f6f7f56aa938db6c9d39782017160781df55
bandarichandana-2926 <bandarichandana96@gmail.com>
2022-11-09 09:47:06 +0530

    task

--- Form-validation/one.js
+++ Form-validation/one.js
@@ -1,7 +1,24 @@
 function myform(){
-    let a1 = document.getElementById('name').value 
+    let name = document.getElementById('username').value 
     
-    if(==""){
-        document.getElementById("usermessage").innerHTML="please enter the name"
+    
+    if(name==""){
+        document.getElementById('usermessage').innerHTML="please enter the name"
+        return false
     }
+    
+    let password=document.getElementById('pass').value
+    let confirmpassword=document.getElementById("repass").value
+    if(password!==confirmpassword){
+         document.getElementById('userpass').innerHTML="please enter correct password"
+         return false
+    }
+    
+    let number = document.getElementById('num').value
+    if(number.length < 10 || number.length > 10){
+        document.getElementById('phone').innerHTML="please enter 10 digit number" 
+        return false
+    
+    }
+   return true
 }
